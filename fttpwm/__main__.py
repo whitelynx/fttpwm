48616b0f6105f2dc93c84e7d2ed6a07e3b4c0f50
David H. Bronke <whitelynx@gmail.com>
2012-05-20 19:49:43 -0500

    Added timestamps to log messages.

--- fttpwm/__main__.py
+++ fttpwm/__main__.py
@@ -14,11 +14,15 @@ from .settings import settings
 
 
 # Monochrome logging
-#logging.basicConfig(level=logging.NOTSET, format="[%(levelname)-8s] %(name)s:  %(message)s")
+#logging.basicConfig(level=logging.NOTSET, datefmt="%H:%M:%S",
+#        format="%(asctime)s[%(levelname)-8s] %(name)s:  %(message)s")
 
 # Color logging (*NIX only)
-logging.basicConfig(level=logging.NOTSET, format="{e}90m[{e}0;1m%(levelname)-8s{e}0;90m]{e}m "
-        "{e}36m%(name)s{e}90m:{e}m  {e}2;3m%(message)s{e}m".format(e='\033['))
+logging.basicConfig(level=logging.NOTSET,
+        datefmt="%H:%M:%S",
+        format="%(asctime)s {e}90m[{e}0;1m%(levelname)-8s{e}0;90m]{e}m {e}36m%(name)s{e}90m:{e}m  "
+            "{e}2;3m%(message)s{e}m".format(e='\033[')
+        )
 
 logger = logging.getLogger("fttpwm")
 

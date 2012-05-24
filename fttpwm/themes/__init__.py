c522b0d800c39095e236a8cce64338b1c51eac60
David H. Bronke <whitelynx@gmail.com>
2012-05-24 08:15:59 -0500

    Started converting DefaultTitlebar into a full theme class.

--- fttpwm/themes/__init__.py
+++ fttpwm/themes/__init__.py
@@ -1,5 +1,6 @@
 from argparse import Namespace
 import logging
+import re
 
 import cairo
 
@@ -7,6 +8,31 @@ from . import fonts, gradients
 
 
 logger = logging.getLogger("fttpwm.themes.gradients")
+
+
+class Color(object):
+    stringFormats = [
+            (lambda x: float(int(x, 16)) / 0xF, re.compile(r'#(?P<r>\d)(?P<g>\d)(?P<b>\d)(?P<a>\d)?$')),
+            (lambda x: float(int(x, 16)) / 0xFF, re.compile(r'#(?P<r>\d{2})(?P<g>\d{2})(?P<b>\d{2})(?P<a>\d{2})?$')),
+            (lambda x: float(int(x, 16)) / 0xFFFF, re.compile(r'#(?P<r>\d{4})(?P<g>\d{4})(?P<b>\d{4})(?P<a>\d{4})?$')),
+            ]
+
+    def __init__(self, string=None):
+        self.r, self.g, self.b, self.a = 0, 0, 0, 1
+
+        if string:
+            for conv, fmt in self.stringFormats:
+                match = fmt.match(string)
+                if match:
+                    self.r, self.g, self.b = map(conv, match.group('r', 'g', 'b'))
+                    if match.group('a') is not None:
+                        self.a = conv(match.group('a'))
+
+    @classmethod
+    def rgb(cls, r, g, b):
+        color = cls()
+        color.r, color.g, color.b = r, g, b
+        return color
 
 
 class Theme(Namespace):
@@ -21,27 +47,46 @@ class Region(Namespace):
     pass
 
 
-class DefaultTitlebar(object):
-    def __init__(self, height=16, textColor=(0, 0, 0), fontFace="drift", fontSize=5,
-            fontSlant=fonts.slant.normal, fontWeight=fonts.weight.normal,
-            bgFrom=(1, 0.9, 0), bgTo=(1, 0.3, 0), bgOrientation=gradients.Direction.vertical
-            ):
-        self.height = height
+class Default(object):
+    normal = dict(
+            titlebarHeight=16,
+            textColor=Color.rgb(0, 0, 0),
+            fontFace="drift",
+            fontSize=5,
+            fontSlant=fonts.slant.normal,
+            fontWeight=fonts.weight.normal,
+            bgFrom=Color.rgb(0.8, 0.7, 0.3),
+            bgTo=Color.rgb(0.8, 0.5, 0.3),
+            bgOrientation=gradients.Direction.vertical,
+            )
+    focused = dict(
+            bgFrom=Color.rgb(1, 0.9, 0),
+            bgTo=Color.rgb(1, 0.3, 0),
+            )
 
-        self.gradient = cairo.LinearGradient(*bgOrientation)
-        gradients.addColorStop(self.gradient, 0, bgFrom)
-        gradients.addColorStop(self.gradient, 1, bgTo)
+    def __init__(self):
+        normalGradient = cairo.LinearGradient(*self.getThemeValue('bgOrientation'))
+        gradients.addColorStop(normalGradient, 0, self.getThemeValue('bgFrom'))
+        gradients.addColorStop(normalGradient, 1, self.getThemeValue('bgTo'))
+        self.normal['gradient'] = normalGradient
 
-        if len(textColor) not in [3, 4]:
-            logger.error("Invalid color: %r", self.textColor)
+        focusedGradient = cairo.LinearGradient(*self.getThemeValue('bgOrientation', True))
+        gradients.addColorStop(focusedGradient, 0, self.getThemeValue('bgFrom', True))
+        gradients.addColorStop(focusedGradient, 1, self.getThemeValue('bgTo', True))
+        self.focused['gradient'] = focusedGradient
 
-        self.textColor = textColor
-        self.fontFace = fontFace
-        self.fontSlant = fontSlant
-        self.fontWeight = fontWeight
-        self.fontSize = fontSize
+    def __getitem__(self, key):
+        return self.getThemeValue(key, self.currentFrame.focused)
 
-    def __call__(self, ctx, frame):
+    def getThemeValue(self, key, focused=False):
+        normalVal = self.normal[key]
+
+        if focused:
+            return self.focused.get(key, normalVal)
+
+        return normalVal
+
+    def paintWindow(self, ctx, frame):
         ctx.set_source(self.gradient)
         ctx.paint()
 

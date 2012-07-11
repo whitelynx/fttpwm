7e7facb98ca51c842e299d2179ce5d4a9689196e
David H. Bronke <whitelynx@gmail.com>
2012-07-11 03:08:00 -0500

    Updated test module to post a notification through the DBus connection.

--- fttpwm/dbus/__main__.py
+++ fttpwm/dbus/__main__.py
@@ -17,3 +17,26 @@ logger = logging.getLogger("fttpwm.dbus.__main__")
 
 bus = connection.SessionBus()
 bus.connect()
+
+
+def onReturn(response):
+    logger.info("Got response %r from notification daemon, with body %r.", response, response.body)
+
+
+bus.callMethod(
+        '/org/freedesktop/Notifications', 'Notify',
+        'susssasa{ss}i',
+        [
+            "fttpwm",           # app_name
+            0,                  # notification_id (spec calls this "replaces_id")
+            "",                 # app_icon
+            "A notification!",  # summary
+            "Text and stuff.",  # body
+            [],                 # actions
+            {},                 # hints
+            -1,                 # expire_timeout
+            ],
+        interface='org.freedesktop.Notifications',
+        destination='org.freedesktop.Notifications',
+        onReturn=onReturn
+        )

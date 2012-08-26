82d41e36a6affe9ed08228af4d8aecf2cbe8d722
David H. Bronke <whitelynx@gmail.com>
2012-08-26 18:00:31 -0500

    Reworked most of LocalObject and DBusInterface; the design and implementation are much better now, and might actually get close to working.

--- fttpwm/dbus/interface.py
+++ fttpwm/dbus/interface.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import unicode_literals, print_function
 """FTTPWM: D-Bus interface definition classes
 
 Copyright (c) 2012 David H. Bronke
 Licensed under the MIT license; see the LICENSE file for details.
 
 """
+from weakref import ref
 
 
 def DBusInterface(interfaceName):
@@ -42,7 +43,71 @@ def DBusInterface(interfaceName):
         """.format(interfaceName)
         _dbus_interfaceName = interfaceName
 
+        @classmethod
+        def _DBusInterface_getMembers(cls):
+            for memberName in dir(cls):
+                yield (memberName, getattr(cls, memberName))
+
+        @classmethod
+        def _DBusInterface_getSignals(cls):
+            for memberName, member in cls._DBusInterface_getMembers():
+                if isinstance(member, _InterfaceSignal):
+                    yield member
+
+        @classmethod
+        def _DBusInterface_getMethods(cls):
+            for memberName, member in cls._DBusInterface_getMembers():
+                if isinstance(member, _InterfaceMethod):
+                    yield member
+
     return _DBusInterface
+
+
+class _LocalInterfaceMethodInfo(object):
+    """D-Bus interface method information, for wrapping local implementation methods.
+
+    """
+    def __init__(self, interfaceMethod, wrapped):
+        self.interfaceMethod = interfaceMethod
+        self.wrapped = wrapped
+
+    def __call__(self, *args, **kwargs):
+        """Call the wrapped method.
+
+        """
+        return self.wrapped(*args, **kwargs)
+
+
+class _RemoteInterfaceMethodInfo(object):
+    """D-Bus interface method information, for wrapping methods on remote objects.
+
+    """
+    def __init__(self, interfaceMethod, obj):
+        self.interfaceMethod = interfaceMethod
+        self.obj = obj
+
+    def __call__(self, *args, **kwargs):
+        """Call this method on the remote object.
+
+        In addition to the arguments accepted by the remote method, the following keyword-only argument is accepted:
+        - `dbus_destination`: Tells the bus to only send the method call to the given connection
+
+        @returns a new `fttpwm.dbus.connection.Callbacks` object for this method call
+
+        """
+        destination = kwargs.pop('dbus_destination', None)
+
+        if len(kwargs) > 0:
+            raise NotImplementedError("Calling methods with explicit keyword arguments is not yet supported!")
+
+        self.obj._dbus_bus.callMethod(
+                self.obj._dbus_path,
+                self.interfaceMethod.methodName,
+                inSignature=self.interfaceMethod.inSignature,
+                args=args,
+                interface=self.interfaceMethod.interfaceName,
+                destination=destination
+                )
 
 
 class _InterfaceMethod(object):
@@ -52,6 +117,7 @@ class _InterfaceMethod(object):
     def __init__(self, func, inSig='', outSig='', resultFields=None):
         self.methodName = func.__name__
         self.__doc__ = func.__doc__
+        self.interface = None
         self.interfaceName = None
 
         self.inSignature = inSig
@@ -62,6 +128,7 @@ class _InterfaceMethod(object):
             self.resultFields = resultFields.split()
 
     def __get__(self, instance, owner):
+        self.interface = ref(owner)
         self.interfaceName = owner._dbus_interfaceName
         return self
 
@@ -69,7 +136,7 @@ class _InterfaceMethod(object):
         return u'{}.{}'.format(self.interfaceName, self.methodName)
 
     def __call__(self, func):
-        raise NotImplementedError
+        return _LocalInterfaceMethodInfo(self, func)
 
 
 def Method(inSig='', outSig='', resultFields=None):
@@ -89,6 +156,37 @@ def Method(inSig='', outSig='', resultFields=None):
     return lambda func: _InterfaceMethod(func, inSig, outSig, resultFields)
 
 
+class _LocalSignalWrapper(object):
+    def __init__(self, signal, preEmit):
+        self.signal = signal
+        self.obj = None
+        self.preEmit = preEmit
+
+    @property
+    def interface(self):
+        return self.signal.interface
+
+    @property
+    def interfaceName(self):
+        return self.signal.interfaceName
+
+    def __get__(self, instance, owner):
+        self.obj = instance
+        return self
+
+    def __call__(self, *args, **kwargs):
+        """Emit this signal from the object it is attached to.
+
+        The `SIGNAL` message will have its `PATH` set to the path of `obj`, its `INTERFACE` set to the name of the
+        interface in which this signal was defined, and `MEMBER` set to the signal's name.
+
+        """
+        if self.preEmit is not None:
+            self.preEmit(*args, **kwargs)
+
+        #FIXME: Implement!
+
+
 class _InterfaceSignal(object):
     """D-Bus interface signal class
 
@@ -96,19 +194,24 @@ class _InterfaceSignal(object):
     def __init__(self, func, sig=''):
         self.signalName = func.__name__
         self.__doc__ = func.__doc__
+        self.interface = None
         self.interfaceName = None
 
         self.signature = sig
 
     def __get__(self, instance, owner):
+        self.interface = ref(owner)
         self.interfaceName = owner._dbus_interfaceName
         return self
 
     def __unicode__(self):
         return u'{}.{}'.format(self.interfaceName, self.signalName)
 
-    def __call__(self, func):
-        raise NotImplementedError
+    def __call__(self, preEmit=None):
+        wrapper = _LocalSignalWrapper(self, preEmit)
+        wrapper.__name__ = '{}'.format(self.signalName)
+        wrapper.__doc__ = self.__doc__
+        return wrapper
 
 
 def Signal(sig=''):
@@ -128,7 +231,7 @@ def Signal(sig=''):
     return lambda func: _InterfaceSignal(func, sig)
 
 
-if __name__ == '__main__':
+def _createSampleInterface():
     class SampleInterface(DBusInterface('com.example.Sample')):
         '''A sample D-Bus interface
 
@@ -151,9 +254,20 @@ if __name__ == '__main__':
 
             '''
 
-    print unicode(SampleInterface), repr(SampleInterface), dir(SampleInterface)
-    print SampleInterface.__doc__
-    print unicode(SampleInterface.LastInputChanged), repr(SampleInterface.LastInputChanged), dir(SampleInterface.LastInputChanged)
-    print SampleInterface.LastInputChanged.__doc__
-    print unicode(SampleInterface.GetLastInput), repr(SampleInterface.GetLastInput), dir(SampleInterface.GetLastInput)
-    print SampleInterface.GetLastInput.__doc__
+    return SampleInterface
+
+
+def test():
+    SampleInterface = _createSampleInterface()
+
+    print(unicode(SampleInterface), repr(SampleInterface), dir(SampleInterface))
+    print(SampleInterface.__doc__)
+    print(unicode(SampleInterface.LastInputChanged), repr(SampleInterface.LastInputChanged),
+            dir(SampleInterface.LastInputChanged))
+    print(SampleInterface.LastInputChanged.__doc__)
+    print(unicode(SampleInterface.GetLastInput), repr(SampleInterface.GetLastInput), dir(SampleInterface.GetLastInput))
+    print(SampleInterface.GetLastInput.__doc__)
+
+
+if __name__ == '__main__':
+    test()
--- fttpwm/dbus/local.py
+++ fttpwm/dbus/local.py
@@ -1,19 +1,93 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import unicode_literals, print_function
 """FTTPWM: D-Bus local object implementation base class
 
 Copyright (c) 2012 David H. Bronke
 Licensed under the MIT license; see the LICENSE file for details.
 
 """
+from collections import defaultdict
+import logging
+from weakref import WeakSet, WeakKeyDictionary, WeakValueDictionary
+
+from .interface import _InterfaceMethodInfo
+from ..utils import naturalJoin
 
 
-class LocalObjectMeta(type):
-    def __new__(mcs, name, bases, dict):
-        for member in dict.keys:
-            dict[member] = dict[member]
+logger = logging.getLogger('fttpwm.dbus.local')
 
-        return type.__new__(mcs, name, bases, dict)
+
+class _MemberWrapper(WeakKeyDictionary):
+    def __init__(self, memberName, members, **overrides):
+        super(_MemberWrapper, self).__init__(
+                (m.interface(), m) for m in members
+                )
+        self.update(overrides)
+
+        self.__name__ = memberName
+
+    def __call__(self, *args, **kwargs):
+        if len(self) == 1:
+            self.values()[0](*args, **kwargs)
+        else:
+            raise TypeError(
+                    '{} has {} overloads in different interfaces! You must call {}[SomeInterface](...) instead.'
+                        .format(self.__name, len(self), self.__name__)
+                    )
+
+
+class _LocalObjectMeta(type):
+    """Metaclass for locally-implemented DBus objects.
+
+    Tracks all implemented interface members, warns about unimplemented interface methods, adds signals from
+    implemented interfaces, and checks for duplicate member names from different interfaces, replacing each such member
+    with a mapping of interface names to members.
+
+    """
+    def __new__(mcs, name, bases, dict_):
+        # Track all implemented interface members.
+        membersByInterface = defaultdict(set)
+        membersByDBusName = defaultdict(set)
+        for member in dict_.values():
+            if isinstance(member, _InterfaceMethodInfo):
+                ifaceMethod = member.interfaceMethod
+
+                membersByInterface[ifaceMethod.interface()].add(ifaceMethod)
+                membersByDBusName[ifaceMethod.methodName].add(ifaceMethod)
+
+        for interface, members in membersByInterface.items():
+            # Check for unimplemented interface members.
+            unimplemented = []
+            for method in interface._DBusInterface_getMethods():
+                if method not in members:
+                    unimplemented.append(method.methodName)
+
+            if len(unimplemented) > 0:
+                logger.warn("Class %r implements the methods %s from interface %r, but is missing %s!",
+                        name,
+                        naturalJoin(m.methodName for m in members),
+                        interface._dbus_interfaceName,
+                        naturalJoin(unimplemented)
+                        )
+
+            # Add signal wrappers for all signals of this interface.
+            for signal in interface._DBusInterface_getSignals():
+                if signal.signalName not in membersByDBusName:
+                    membersByDBusName[signal.signalName].add(signal())
+
+        dict_['_dbus_interfaces'] = WeakSet(membersByInterface.keys())
+
+        interfaceMembersByName = WeakValueDictionary()
+        for memberName, members in membersByDBusName.items():
+            print('members with name {!r} present in interfaces: {}'.format(
+                    memberName, ', '.join(member.interfaceName for member in members)))
+            dict_[memberName] = _MemberWrapper(memberName, members)
+
+            interfaceMembersByName[memberName] = members
+
+        dict_['_dbus_interfaceMembersByName'] = interfaceMembersByName
+
+        return type.__new__(mcs, name, bases, dict_)
 
 
 class LocalObject(object):
@@ -29,7 +103,7 @@ class LocalObject(object):
                 self._last_input = None
 
             @SampleInterface.StringifyVariant
-            def StringifyVariant(self, var):
+            def SampleInterface_StringifyVariant(self, var):
                 '''Turn the given Variant into a String.
 
                 '''
@@ -37,7 +111,7 @@ class LocalObject(object):
                 return str(var)
 
             @SampleInterface.LastInputChanged
-            def LastInputChanged(self, var):
+            def SampleInterface_LastInputChanged(self, var):
                 '''Emitted whenever StringifyVariant gets called with a different input.
 
                 '''
@@ -45,11 +119,63 @@ class LocalObject(object):
                 self._last_input = var
 
             @SampleInterface.GetLastInput
-            def GetLastInput(self):
+            def SampleInterface_GetLastInput(self):
                 '''Get the last value passed to StringifyVariant.
 
                 '''
                 return self._last_input
 
     """
-    __metaclass__ = LocalObjectMeta
+    __metaclass__ = _LocalObjectMeta
+
+    def __init__(self, object_path, bus):
+        self._dbus_path = object_path
+        self._dbus_bus = bus
+
+
+def test():
+    from .interface import _createSampleInterface
+    from .. import singletons
+
+    SampleInterface = _createSampleInterface()
+
+    class Example(LocalObject):
+        def __init__(self, object_path, bus=None):
+            if bus is None:
+                bus = singletons.dbusSessionBus
+            super(Example, self).__init__(object_path, bus)
+            self._last_input = None
+
+        @SampleInterface.StringifyVariant
+        def SampleInterface_StringifyVariant(self, var):
+            '''Turn the given Variant into a String.
+
+            '''
+            self.LastInputChanged(var)  # emits the signal
+            return str(var)
+
+        @SampleInterface.LastInputChanged
+        def SampleInterface_LastInputChanged(self, var):
+            '''Emitted whenever StringifyVariant gets called with a different input.
+
+            '''
+            # This is run just before the signal is actually emitted; just use 'pass' if nothing should happen.
+            self._last_input = var
+
+        @SampleInterface.GetLastInput
+        def SampleInterface_GetLastInput(self):
+            '''Get the last value passed to StringifyVariant.
+
+            '''
+            return self._last_input
+
+    print(unicode(Example), repr(Example), dir(Example))
+    print(Example.__doc__)
+
+    example = Example('/com/foo/bar')
+    print(unicode(example), repr(example), dir(example))
+    print(example.__doc__)
+    print(unicode(example.LastInputChanged), repr(example.LastInputChanged), dir(example.LastInputChanged))
+    print(example.LastInputChanged.__doc__)
+    print(unicode(example.GetLastInput), repr(example.GetLastInput), dir(example.GetLastInput))
+    print(example.GetLastInput.__doc__)

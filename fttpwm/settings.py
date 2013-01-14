1f77d164f0a8a0285af2dfcc4e9d1c7e054a89d5
David H. Bronke <whitelynx@gmail.com>
2013-01-14 02:34:31 -0600

    Updated Settings to store its configuration in a separate dict object, in order to avoid name collisions with config values.

--- fttpwm/settings.py
+++ fttpwm/settings.py
@@ -7,14 +7,17 @@ from .xdg.basedir import config as config_dirs
 logger = logging.getLogger("fttpwm.settings")
 
 
-class Settings(dict):
+class Settings(object):
     __defaultConfigFile = join(dirname(__file__), "default_config.py")
 
     __defaultSettings = dict()
 
+    def __init__(self):
+        self.__settings = dict()
+
     def __getattr__(self, key):
         try:
-            return self[key]
+            return self.__settings[key]
         except KeyError:
             return self.__defaultSettings[key]
 
@@ -23,14 +26,14 @@ class Settings(dict):
 
     def loadSettings(self):
         logger.info("Loading settings...")
-        self.clear()
+        self.__settings.clear()
 
         # Load each settings file, overwriting lower-priority settings with higher-priority ones.
         for filename in reversed(config_dirs.findAllFiles("fttpwm/config.py") + [self.__defaultConfigFile]):
             if exists(filename):
                 # We've found a settings file; load it.
                 logger.debug("Found settings file: %s", filename)
-                execfile(filename, self)
+                execfile(filename, self.__settings)
 
         logger.info("Finished loading settings.")
 
